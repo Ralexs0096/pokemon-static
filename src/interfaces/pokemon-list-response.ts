031c46ed76c6d1d53fbbe7acfa08447f341832f8
Alexs Ruiz <ralexs.acu@gmail.com>
2024-09-18 22:59:42 -0600

    Add Prettier plugin and initial config

--- src/interfaces/pokemon-list-response.ts
+++ src/interfaces/pokemon-list-response.ts
@@ -1,11 +1,11 @@
 export interface PokemonListResponse {
-  count:    number;
-  next:     string;
+  count: number;
+  next: string;
   previous: null;
-  results:  Result[];
+  results: Result[];
 }
 
 export interface Result {
   name: string;
-  url:  string;
+  url: string;
 }
